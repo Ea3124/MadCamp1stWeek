3fd9842387cb8bebfea3f021c9d207edca178dae
Ea3124 <leesj6717@naver.com>
2024-12-30 17:14:30 +0900

    feat: signup UI구현

--- app/src/main/java/com/example/madcamp1stweek/SignupFragment.kt
+++ app/src/main/java/com/example/madcamp1stweek/SignupFragment.kt
@@ -2,6 +2,8 @@
 package com.example.madcamp1stweek
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -18,6 +20,7 @@ class SignupFragment : DialogFragment() {
     private lateinit var editTextNickName: EditText
     private lateinit var editTextID: EditText
     private lateinit var editTextPassword: EditText
+    private lateinit var editTextPasswordConfirm: EditText  // 비밀번호 확인 필드 추가
     private lateinit var buttonSignUp: Button
     private lateinit var buttonCancel: Button
 
@@ -47,6 +50,7 @@ class SignupFragment : DialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 배경 투명 설정
         val view = inflater.inflate(R.layout.fragment_signup, container, false)
         dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         return view
@@ -59,6 +63,7 @@ class SignupFragment : DialogFragment() {
         editTextNickName = view.findViewById(R.id.editTextNickName)
         editTextID = view.findViewById(R.id.editTextID)
         editTextPassword = view.findViewById(R.id.editTextPassword)
+        editTextPasswordConfirm = view.findViewById(R.id.editTextPasswordConfirm)
         buttonSignUp = view.findViewById(R.id.buttonSignUp)
         buttonCancel = view.findViewById(R.id.buttonCancel)
 
@@ -70,10 +75,13 @@ class SignupFragment : DialogFragment() {
             val nickName = editTextNickName.text.toString().trim()
             val id = editTextID.text.toString().trim()
             val password = editTextPassword.text.toString().trim()
+            val passwordConfirm = editTextPasswordConfirm.text.toString().trim()  // 비밀번호 확인 값 가져오기
 
-            if (nickName.isEmpty() || id.isEmpty() || password.isEmpty()) {
+            if (nickName.isEmpty() || id.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty()) {
                 Toast.makeText(requireContext(), "모든 필드를 입력해주세요.", Toast.LENGTH_SHORT).show()
-            } else {
+            } else if (password != passwordConfirm) {  // 비밀번호 일치 여부 확인
+                Toast.makeText(requireContext(), "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            }else {
                 coroutineScope.launch {
                     val existingUser = withContext(Dispatchers.IO) {
                         userDao.getUserById(id)
